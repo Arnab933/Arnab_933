0287db1230140a0cf120e6d93247edffda6ae526
Arnab Pratihar <128636826+Arnab933@users.noreply.github.com>
2024-08-10 21:27:49 +0530

    Update Int_pali.java

--- Java/Int_pali.java
+++ Java/Int_pali.java
@@ -3,7 +3,7 @@ public class Int_pali {
     public static boolean check_pali(int a) {
         String s = String.valueOf(a);
         // convert the int(121) value into String("121")
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length()/2; i++) {
             if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
                 /* charAt(index)------is a method that returns the character on an index */
                 return false;
